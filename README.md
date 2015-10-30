025139f14fdd03bb48e4a08c7fa2c55e17de71ae
Adam Dierkens <adam@dierkens.com>
2015-10-29 23:48:21 -0700

    Begin porting APIs
    Signed-off-by: Adam Dierkens <adam@dierkens.com>

---
+++ README.md
@@ -0,0 +1,44 @@
+# fantasy-football-nerd
+> An API wrapper for fantasyfootballnerd.com
+
+## Getting Started
+
+```shell
+npm install --save fantasy-football-nerd
+```
+
+```js
+var FFNerd = require('fantasy-football-nerd');
+var ff = FFNerd({ api_key: <Your API Key> });
+
+ff.teams(function(teams){
+    console.log('Got teams');
+});
+
+```
+
+## API Progress
+
+The full API can be seen [here](http://www.fantasyfootballnerd.com/fantasy-football-api)
+
+- [x] NFL Teams
+- [x] NFL Schedule
+- [x] NFL Players
+- [x] Bye Weeks
+- [x] Injuries
+- [x] Weather Forecasts
+- [x] Auction Values
+- [ ] Draft Rankings
+- [ ] Draft Projections
+- [ ] Weekly Rankings
+- [ ] Weekly Projections
+- [ ] Weekly IDP Rankings
+- [ ] NFL Picks
+- [ ] Depth Charts
+- [ ] Defensive Rankings
+- [ ] Defensive Rankings
+- [ ] Game Day Inactives
+- [ ] Player Stats & Info
+- [ ] Daily Fantasy Football
+
+
---
+++ index.js
@@ -0,0 +1,104 @@
+'use strict';
+var request = require('request');
+var _ = require('lodash');
+
+function optionsHelper(opt, callback) {
+    if (!callback) {
+        callback = opt;
+        opt = null;
+    }
+
+    req_options = {}
+    if (opt) {
+        req_options.options = [ opt ];
+    }
+
+    return {
+        callback: callback,
+        options: req_options
+    }
+}
+
+class FFNerd {    
+    constructor(options) {
+        default_options = {
+            url: 'http://fantasyfootballnerd.com/service/',
+            format: 'json',
+        }
+        
+        if (!options.api_key) {
+            throw "api_key is required";
+        }
+
+        this.options = _.assign(default_options, options);
+    }
+
+    sendServiceRequest(config, callback) {
+        callback = callback || function(){};
+        var url = this.options.url + config.service + '/' + this.options.format + '/' + this.options.api_key;
+        if (config.options) {
+            for (opt_i in config.options) {
+                var opt = config.options[opt_i];
+                url += '/' + opt
+            }
+        }
+        request(url, function(error, response, body) {
+            if (response.statusCode != 200) {
+                if (error) {
+                    callback(error, null);
+                } else {
+                    callback(body, null);
+                }
+            } else {
+                callback(null, JSON.parse(body));
+            }
+        });       
+    }
+    
+    teams(callback) {
+        this.sendServiceRequest({ service: 'nfl-teams' }, callback);
+    }
+
+    schedule(callback) {
+        this.sendServiceRequest({ service: 'schedule' }, callback);
+    }
+
+    players(position, callback) {
+        var resolved = optionsHelper(position, callback);
+        var req_options = resolved.options;
+        callback = resolved.callback;
+        
+        req_options.service = 'players';
+
+        this.sendServiceRequest(req_options, callback);
+    }
+
+    byes(callback) {
+        this.sendServiceRequest({ service: 'byes' }, callback);
+    }
+
+    injuries(week, callback) {
+        var resolved = optionsHelper(week, callback);
+        var req_options = resolved.options;
+        callback = resolved.callback;
+        
+        req_options.service = 'injuries';
+
+        this.sendServiceRequest(req_options, callback);
+    }
+
+    weather(callback) {
+        this.sendServiceRequest({ service: 'weather' }, callback);
+    }
+
+    auction(ppr, callback) {
+        var resolved = optionsHelper(ppr, callback);
+        var req_options = resolved.options;
+        callback = resolved.callback;
+
+        req_options.service = 'auction';
+
+        this.sendServiceRequest(req_options, callback);
+}
+
+module.exports = FFNerd;
